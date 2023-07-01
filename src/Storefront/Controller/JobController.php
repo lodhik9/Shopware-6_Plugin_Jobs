e8cf94e9e26bcb38a31c88dc7afea1eca907d94a
Muhammad  Lodhi <lodhik9@gmail.com>
2023-07-01 22:54:29 +0200

    Second Commit

--- src/Storefront/Controller/JobController.php
+++ src/Storefront/Controller/JobController.php
@@ -3,20 +3,33 @@
 namespace Swag\JobExampleSecond\Storefront\Controller;
 
 use Shopware\Storefront\Controller\StorefrontController;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Shopware\Core\System\SalesChannel\SalesChannelContext;
+use Shopware\Storefront\Page\GenericPageLoaderInterface;
 
 /**
  * @Route(defaults={"_routeScope"={"storefront"}})
  */
 class JobController extends StorefrontController
 {
+    private GenericPageLoaderInterface $genericPageLoader;
+
+    public function __construct(GenericPageLoaderInterface $genericPageLoader) {
+        $this->genericPageLoader = $genericPageLoader;
+        
+    }
     /**
     * @Route("/jobs", name="frontend.jobs", methods={"GET"})
     */
-    public function showJobs(): Response
+    public function showJobs(Request $request, SalesChannelContext $context): Response
     {
-        return $this->renderStorefront('@SwagJobExampleSecond/storefront/page/jobs.html.twig');
+        $page = $this->genericPageLoader->load($request,$context);
+//        dd($page);
+        return $this->renderStorefront('@SwagJobExampleSecond/storefront/page/jobs.html.twig', [
+            'heading' => 'Welcome to Jobs Portal',
+            'page' => $page
+        ]);
     }
 }
